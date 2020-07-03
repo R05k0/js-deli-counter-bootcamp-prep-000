4d2315aa2ba5597d9b3c7e1b44879c0c4f57a88c
Scott Rodriguez <scottmrodriguez51@gmail.com>
2020-07-03 13:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,17 @@ function nowServing(line) {
   }
 
   return `Currently serving ${line.shift()}.`
-};
+}
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+}
