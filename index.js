4f9616b2cd0aac79c158bce191df42d73d3618b3
Scott Rodriguez <scottmrodriguez51@gmail.com>
2020-07-03 13:32:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,3 @@ function takeNumber(line, name) {
  
   return (`Welcome ${name}. You are number ${line} in line.`)
 }
-function nowServing(line) {
-  if(!line.length) {
-    
-    return ("There is nobody waiting to be served")
-  }
-}
